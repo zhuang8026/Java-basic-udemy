15666be46254b229e426a4e4394ea37457a2b8ae
WILLIAM_CHUANG <zhuang8026@gmail.com>
2021-05-13 15:33:29 +0800

    [9-數組]

--- src/main/java/onedimensionalarray/ArrayDemo1.java
+++ src/main/java/onedimensionalarray/ArrayDemo1.java
@@ -3,22 +3,24 @@ package onedimensionalarray;
 public class ArrayDemo1 {
 
   public static void main(String[] args) {
-    double[] myList = new double[10];
+//    double[] myList = new double[10];
+//    myList[0] = 5.6;
+//    myList[1] = 4.5;
+//    myList[2] = 3.3;
 
-    myList[0] = 5.6;
-    myList[1] = 4.5;
-    myList[2] = 3.3;
-    myList[3] = 13.2;
-    myList[4] = 4.0;
-    myList[5] = 34.33;
-    myList[6] = 34.0;
-    myList[7] = 45.45;
-    myList[8] = 99.993;
-    myList[9] = 11123;
+     double[] myList = { 5.6, 4.5, 3.3 }; // 等價於上面
 
-    System.out.println(myList[5]);
+    System.out.println(myList[2]);
 
+    int[] num = new int[3];
+    System.out.println("default：" + num[0]); // 0 (default)
+    num[0] = 1;
+    num[1] = 2;
+    num[2] = 3;
 
+    for (int i : num) {
+      System.out.println(i);
+    }
   }
 
 }
--- src/main/java/onedimensionalarray/ArrayDemo3.java
+++ src/main/java/onedimensionalarray/ArrayDemo3.java
@@ -3,7 +3,7 @@ package onedimensionalarray;
 public class ArrayDemo3 {
 
   public static void main(String[] args) {
-    double[] myList = {1.9, 2.9, 3.9};
+    double[] myList = {1.9, 2.9, 3.9}; // 初始化數組
     System.out.println(myList.length);
     System.out.println(myList[2]);
 

b8941bd95b947e19f387f282e8a36e0c8972653a
WILLIAM_CHUANG <zhuang8026@gmail.com>
2021-07-13 15:02:18 +0800

    [類加載器] Bootstarp Extension System 自定義類

--- src/main/java/datastructure/stack/StackDemo.java
+++ src/main/java/datastructure/stack/StackDemo.java
@@ -19,19 +19,18 @@ public class StackDemo {
 
     System.out.println(stack); // [1, 2, 3, 4, 5, 6]
 
-
     System.out.println(stack.pop()); // 6 - 只取得，且抽離 items
-
-    System.out.println(stack);
+    System.out.println(stack); // [1, 2, 3, 4, 5]
 
     System.out.println(stack.peek()); // 5 -  只取得，但不抽離 items
-    System.out.println(stack);
+    System.out.println(stack); // [1, 2, 3, 4, 5]
 
     while (!stack.isEmpty()){ // 抽離全部 參數
-      System.out.println(stack.pop());
+      System.out.println(stack.isEmpty()); // false
+      System.out.println(stack.pop()); // 取得最後一個參數並刪除
     }
 
-    System.out.println(stack);
+    System.out.println(stack); // [] 因為上面已經刪空了
 
   }
 }
